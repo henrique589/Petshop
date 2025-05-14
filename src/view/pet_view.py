1a55a6fd9045fb6ef4e40466a1c395ba0563cf3d
dudispires <edumcpires@aluno.ufsj.edu.br>
2025-05-14 15:32:39 -0300

    ✨ Pet view

--- src/view/pet_view.py
+++ src/view/pet_view.py
@@ -3,15 +3,18 @@ import sqlite3
 def solicitar_dados_pet():
     nome = input("Nome: ")
     nome_dono= input("Nome do dono: ")
+    resultado_consulta= consultar_nome_dono(nome_dono)
     
+    if resultado_consulta:
+        raca = input("Raça: ")
+        idade = input("Idade: ")
+        peso = input("Peso: ")
+        tipo_animal = input("Tipo do animal: ")
+        return nome, nome_dono, raca, idade, peso, tipo_animal
     
-    if resultado:
-
-    raca = input("Raça: ")
-    idade = input("Idade: ")
-    peso = input("Peso: ")
-    tipo_animal = input("Tipo do animal: ")
-    return nome, nome_dono, raca, idade, peso, tipo_animal
+    else:
+        print("O dono inserido não está cadastrado, realize o cadastro e tente novamente.")
+        return None
 
 
 def consultar_nome_dono(nome_dono):
@@ -20,3 +23,5 @@ def consultar_nome_dono(nome_dono):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM pets WHERE nome = ?", (nome_dono,))
     resultado= cursor.fetchone()
+    conn.close()
+    return resultado
