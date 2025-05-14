578aa0a3ebc1e033f5f8eaeea85e0e199195dcb5
dudispires <edumcpires@aluno.ufsj.edu.br>
2025-05-14 15:28:06 -0300

    ✨ Cadastro dos dados pet

---
+++ src/database/pet_dao.py
@@ -0,0 +1,16 @@
+import sqlite3
+from model.pet import Pet
+
+class PetDAO:
+    def __init__(self, db_path='petshop.db'):
+        self.db_path = db_path
+
+    def salvar(self, pet: Pet):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO pets (nome, nome_dono, raca, idade, peso, tipo_animal)
+            VALUES (?, ?, ?, ?)
+        ''', (pet.nome,pet.nome_dono, pet.raca, pet.idade, pet.tipo_animal))
+        conn.commit()
+        conn.close()
