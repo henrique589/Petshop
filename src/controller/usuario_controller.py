31d64c393ca97f1739f01da8dd4abc3a08e01f21
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2025-05-15 21:30:07 -0300

    :white_check_mark: Classe UsuarioController

--- src/controller/usuario_controller.py
+++ src/controller/usuario_controller.py
@@ -5,24 +5,26 @@ from database.cliente_dao import ClienteDAO
 from view.usuario_view import solicitar_dados_usuario, solicitar_login
 from view.cliente_view import solicitar_dados_cliente
 
-def cadastrar_cliente():
-    nome, email, senha = solicitar_dados_usuario()
-    usuario = Usuario(nome, email, senha, tipo='cliente')
-    usuario_dao = UsuarioDAO()
-    usuario_id = usuario_dao.cadastrar_usuario_retornaid(usuario)
-    telefone, cpf = solicitar_dados_cliente()
-    cliente = Cliente(usuario_id, telefone, cpf)
-    clienteDao = ClienteDAO()
-    clienteDao.salvar(cliente)
-    print(f'✅ Cadastro realizado com sucesso!')   
+class UsuarioController:
+    def __init__(self):
+        self.usuarioDao = UsuarioDAO()
 
-def login_usuario():
-    email, senha = solicitar_login()
-    dao = UsuarioDAO()
-    resultado = dao.autenticar_usuario(email, senha)
-    if resultado:
-        print(f'✅ Login realizado com sucesso.')
-        return resultado['tipo']
-    else:
-        print(f'❌ Credenciais inválidas.')
-        return None
+    def cadastrar_cliente(self):
+        nome, email, senha = solicitar_dados_usuario()
+        usuario = Usuario(nome, email, senha, tipo='cliente')
+        usuario_id = self.usuarioDao.cadastrar_usuario_retornaid(usuario)
+        telefone, cpf = solicitar_dados_cliente()
+        cliente = Cliente(usuario_id, telefone, cpf)
+        clienteDao = ClienteDAO()
+        clienteDao.salvar(cliente)
+        print(f'✅ Cadastro realizado com sucesso!')   
+
+    def login_usuario(self):
+        email, senha = solicitar_login()
+        resultado = self.usuarioDao.autenticar_usuario(email, senha)
+        if resultado:
+            print(f'✅ Login realizado com sucesso.')
+            return resultado['tipo']
+        else:
+            print(f'❌ Credenciais inválidas.')
+            return None
