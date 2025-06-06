61b5d63841be0cbdbccf624a20ba80ff78c7ce9e
Gabriel Souza <gabrielsouza010208@gmail.com>
2025-06-06 13:28:22 -0300

    :sparkles: TF-10: Adiciona funcionalidade de consulta de serviços pelo cliente

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, session, send_file
+from flask import Flask, request, redirect, url_for, session, send_file, render_template
 from sqlite3 import IntegrityError
 from controller.usuario_controller import UsuarioController
 from controller.pet_controller import PetController
@@ -19,6 +19,7 @@ produto_controller = ProdutoController()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 HTML_DIR = os.path.join(BASE_DIR, 'static')
 
+
 @app.route('/')
 def index():
     if 'usuario' in session:
@@ -28,9 +29,11 @@ def index():
             return redirect('/painel-funcionario')
     return send_file(os.path.join(HTML_DIR, 'login.html'))
 
+
 @app.route('/cadastro')
 def cadastro_get():
     return send_file(os.path.join(HTML_DIR, 'cadastro.html'))
+
 
 @app.route('/cadastro', methods=['POST'])
 def cadastro_post():
@@ -45,6 +48,7 @@ def cadastro_post():
         return "Cliente cadastrado com sucesso! <a href='/'>Voltar para login</a>"
     except IntegrityError:
         return redirect("/cadastro?erro=email")
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -64,14 +68,17 @@ def login():
             return redirect('/painel-gerente')
     return "Usuário ou senha inválidos. <a href='/'>Tentar novamente</a>"
 
+
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
 
+
 @app.route('/cadastro-pet')
 def cadastro_pet_get():
     return send_file(os.path.join(HTML_DIR, 'cadastro_pet.html'))
+
 
 @app.route('/cadastro-pet', methods=['POST'])
 def cadastro_pet_post():
@@ -85,8 +92,10 @@ def cadastro_pet_post():
     peso = float(request.form['peso'])
     tipo_animal = request.form['tipo_animal']
 
-    pet_controller.cadastrar_pet_web(nome, email_dono, raca, idade, peso, tipo_animal)
+    pet_controller.cadastrar_pet_web(
+        nome, email_dono, raca, idade, peso, tipo_animal)
     return '', 204
+
 
 @app.route('/editar-pet', methods=['POST'])
 def editar_pet():
@@ -103,6 +112,7 @@ def editar_pet():
     pet_controller.editar_pet_web(pet_id, nome, raca, idade, peso, tipo_animal)
     return '', 204
 
+
 @app.route('/excluir-pet', methods=['POST'])
 def excluir_pet():
     if 'usuario' not in session:
@@ -112,11 +122,13 @@ def excluir_pet():
     pet_controller.excluir_pet_web(pet_id)
     return '', 204
 
+
 @app.route('/perfil')
 def perfil_cliente():
     if 'usuario' not in session:
         return redirect('/')
     return send_file(os.path.join(HTML_DIR, 'perfil.html'))
+
 
 @app.route('/api/pets')
 def api_listar_pets():
@@ -127,17 +139,20 @@ def api_listar_pets():
     pets = pet_controller.listar_pets_por_email(email)
     return pets
 
+
 @app.route('/painel-gerente')
 def painel_gerente():
     if 'usuario' not in session or session['tipo'] != 'gerente':
         return redirect('/')
     return send_file(os.path.join(HTML_DIR, 'painel_gerente.html'))
 
+
 @app.route('/api/usuarios')
 def api_listar_usuarios():
     if 'usuario' not in session or session['tipo'] != 'gerente':
         return {"erro": "Não autorizado"}, 401
     return usuario_controller.listar_usuarios()
+
 
 @app.route('/api/usuarios', methods=['POST'])
 def api_criar_usuario():
@@ -151,6 +166,7 @@ def api_criar_usuario():
 
     usuario_controller.criar_usuario(nome, email, senha, tipo)
     return '', 204
+
 
 @app.route('/api/editar-usuario', methods=['POST'])
 def api_editar_usuario():
@@ -166,6 +182,7 @@ def api_editar_usuario():
     usuario_controller.editar_usuario(usuario_id, nome, email, senha, tipo)
     return '', 204
 
+
 @app.route('/api/excluir-usuario', methods=['POST'])
 def api_excluir_usuario():
     if 'usuario' not in session or session['tipo'] != 'gerente':
@@ -175,11 +192,15 @@ def api_excluir_usuario():
     usuario_controller.excluir_usuario(usuario_id)
     return '', 204
 
+
 @app.route('/api/servicos')
 def api_listar_servicos():
-    if 'usuario' not in session or session['tipo'] != 'gerente':
-        return {"erro": "Não autorizado"}, 401
+    # Permitir o cliente a listar os serviços também
+    if 'usuario' not in session or session.get('tipo') not in ['gerente', 'cliente']:
+        return {"erro": "Não autorizado"}, 403
+
     return servico_controller.listar_servicos()
+
 
 @app.route('/api/servicos', methods=['POST'])
 def api_criar_servico():
@@ -191,8 +212,10 @@ def api_criar_servico():
     preco = float(request.form['preco'])
     estoque = int(request.form['estoque'])
 
-    servico_controller.dao.adicionar(Servico(nome=nome, descricao=descricao, preco=preco, estoque=estoque))
+    servico_controller.dao.adicionar(
+        Servico(nome=nome, descricao=descricao, preco=preco, estoque=estoque))
     return '', 204
+
 
 @app.route('/api/editar-servico', methods=['POST'])
 def api_editar_servico():
@@ -205,8 +228,10 @@ def api_editar_servico():
     preco = float(request.form['preco'])
     estoque = int(request.form['estoque'])
 
-    servico_controller.dao.atualizar(Servico(id=id_servico, nome=nome, descricao=descricao, preco=preco, estoque=estoque))
+    servico_controller.dao.atualizar(Servico(
+        id=id_servico, nome=nome, descricao=descricao, preco=preco, estoque=estoque))
     return '', 204
+
 
 @app.route('/api/excluir-servico', methods=['POST'])
 def api_excluir_servico():
@@ -217,17 +242,20 @@ def api_excluir_servico():
     servico_controller.dao.remover(id_servico)
     return '', 204
 
+
 @app.route('/painel-servicos')
 def painel_servicos():
     if 'usuario' not in session or session['tipo'] != 'gerente':
         return redirect('/')
     return send_file(os.path.join(HTML_DIR, 'painel_servicos.html'))
 
+
 @app.route('/painel-produtos')
 def painel_produtos():
     if 'usuario' not in session or session['tipo'] != 'gerente':
         return redirect('/')
     return send_file(os.path.join(HTML_DIR, 'painel_produtos.html'))
+
 
 @app.route('/api/produtos')
 def api_listar_produtos():
@@ -246,6 +274,7 @@ def api_listar_produtos():
         for p in produtos
     ]
 
+
 @app.route('/api/produtos', methods=['POST'])
 def api_criar_produto():
     if 'usuario' not in session or session['tipo'] != 'gerente':
@@ -256,9 +285,11 @@ def api_criar_produto():
     preco = float(request.form['preco'])
     estoque = int(request.form['estoque'])
 
-    produto = Produto(nome=nome, descricao=descricao, preco=preco, estoque=estoque)
+    produto = Produto(nome=nome, descricao=descricao,
+                      preco=preco, estoque=estoque)
     produto_controller.dao.adicionar(produto)
     return '', 204
+
 
 @app.route('/api/editar-produto', methods=['POST'])
 def api_editar_produto():
@@ -271,9 +302,11 @@ def api_editar_produto():
     preco = float(request.form['preco'])
     estoque = int(request.form['estoque'])
 
-    produto = Produto(id=id_produto, nome=nome, descricao=descricao, preco=preco, estoque=estoque)
+    produto = Produto(id=id_produto, nome=nome,
+                      descricao=descricao, preco=preco, estoque=estoque)
     produto_controller.dao.atualizar(produto)
     return '', 204
+
 
 @app.route('/api/excluir-produto', methods=['POST'])
 def api_excluir_produto():
@@ -283,6 +316,13 @@ def api_excluir_produto():
     id_produto = int(request.form['id'])
     produto_controller.dao.remover(id_produto)
     return '', 204
+
+
+@app.route('/servicos-cliente')
+def servicos_cliente():
+    if 'usuario' not in session or session.get('tipo') != 'cliente':
+        return redirect('/')
+    return send_file(os.path.join(HTML_DIR, 'servicos_cliente.html'))
 
 
 if __name__ == '__main__':
